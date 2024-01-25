c54c798517967d19682fa31b007015a0e0c295d3
YuraKolodiy13 <timber2017@gmail.com>
2024-01-25 20:15:07 +0100

    try to connect to boardgame.io

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -34,7 +34,9 @@ export default function Game({ auth, setAuth }) {
   const App = Client({
     game: Buzzer,
     board: Table,
-    multiplayer: SocketIO({ server: 'https://multibuzzer-qrcode.vercel.app/api/' }),
+    multiplayer: SocketIO({
+      server: 'https://multibuzzer-qrcode.vercel.app/api/',
+    }),
     debug: false,
     loading: loadingComponent,
   });
